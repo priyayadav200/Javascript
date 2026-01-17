49ca37328d2d675d5c734254546edf7de2a89c86
PriyaYadav <iitianpriya2003@gmail.com>
2026-01-17 11:22:09 +0000

    commit

---
+++ 01basic/Variable.js
@@ -0,0 +1,19 @@
+const accountId = 123344 ;
+
+var emailId = "Akshu@gmail.com";
+
+let accountPassword = "56789";
+
+accountCity = "jaipur";
+
+emailId = "Lakshika@gmail.com" ;
+
+let account= "7890" ;
+var email = "pppp"
+accountCity = "Ajmer" ;
+
+// accountId = 2 ;
+
+
+console.log(accountId); 
+console.table([accountId , emailId , accountPassword , accountCity , account , email])
