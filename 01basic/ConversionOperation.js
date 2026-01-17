905f5f402417d98f29bb79ed45ffbebcfaa0a045
PriyaYadav <iitianpriya2003@gmail.com>
2026-01-17 12:44:11 +0000

    conversion of datatype

---
+++ 01basic/ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33aaa";
+// let score = null ;
+// let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "prishu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let NumbertoString = String(someNumber)
+// console.log(typeof NumbertoString )
+// console.log(NumbertoString);
+
+
+
+/*********** Operations ***************/
