be2de5e748cd22abea3b9dbc4dc1298315d59abf
PriyaYadav <iitianpriya2003@gmail.com>
2026-01-18 08:50:26 +0000

    Conversions

--- 01basic/ConversionOperation.js
+++ 01basic/ConversionOperation.js
@@ -26,3 +26,52 @@ let NumbertoString = String(someNumber)
 
 
 /*********** Operations ***************/
+let value = 4 ;
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "hello"
+let str2 =  " life"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2")
+// console.log("2" + 1)
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3 = 2+2
+
+
+let counter = 100
+counter++
+console.log(counter)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+
+
