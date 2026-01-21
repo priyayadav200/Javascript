daed24c2d114c96609b48dfa31cc83a9a03fbcd2
PriyaYadav <iitianpriya2003@gmail.com>
2026-01-21 10:14:52 +0000

    String

---
+++ 01basic/String.js
@@ -0,0 +1,18 @@
+const name = "Priya Yadav "
+const repoCount = 7 ;
+
+// old way
+console.log(name + repoCount);
+
+// new way 
+console.log(`the name is ${name} and the repository count is ${repoCount}`);
+
+const gameName = new String("Hello priya");
+
+
+
+
+
+
+
+
